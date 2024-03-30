7b7b21ced4cf855b93b3553bb431fdcfc7107628
drekkg <rizzagum@gmail.com>
2024-03-30 19:58:50 +0100

    FEAT adds basic validate email function

--- run.py
+++ run.py
@@ -1,5 +1,16 @@
 #Main Python file
+# import pyfiglet module 
+import pyfiglet 
 
+title = pyfiglet.figlet_format(" * * * *", font = "5lineoblique" ) 
 
-print("Welcome to the Coldroom Capacity Calculator \n")
-print("Please enter Your email address: ")
+print(f"{title}* Welcome to the Coldroom Calculator * \n")
+
+def validate_email():
+    while True:
+        email = input(" Please enter Your email address to continue: \n")
+        if email == "q":
+            break
+        
+    
+validate_email()
