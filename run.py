1ed0fc32e0d62203e146b90cca947cc15b4b55f4
drekkg <rizzagum@gmail.com>
2024-04-01 11:37:29 +0200

    FEAT adds func to see if new email is already registered

--- run.py
+++ run.py
@@ -1,17 +1,10 @@
 #Main Python file
 
-# import pyfiglet module command line font creator library
+# import the various libraries
 import pyfiglet 
-
-#import the gspread online spreadsheet
 import gspread
 from google.oauth2.service_account import Credentials
-
-
-# import os library to clear screen in console
 import os
-
-# import email validator
 from email_validator import validate_email, EmailNotValidError
 
 # create the scope for the connected APIs
@@ -19,6 +12,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
+    
     ]
 # create constant vars for validation of connected APIs
 CREDS = Credentials.from_service_account_file("creds.json")
@@ -36,9 +30,10 @@ print(f"{title}* Welcome to the Coldroom Calculator * \n")
 
 def clear():
     """
-    Clear screen function
+    Function to clear the console
     """
     os.system("cls" if os.name == "nt" else "clear")
+    
 
 def check_email_registered():
     """
@@ -58,35 +53,52 @@ def check_email_registered():
         # get user email and check if they are registered, quit or open the register module
         user_email = input("Please enter your registered email address, \n 'r' to register or 'q' to quit: \n")
         if user_email in mail:
-            print("Loading Projects......\n")
+            print("Loading your Projects......\n")
+            
+            #open projects module
+            
+            
             break
         elif user_email == "q":
             print("See you soon, Stay Cool")
             break
         elif user_email == "r":
             print("Loading the register page.......")
-            register_user()
+            register_user(mail)
             break
         else:
             print(f"{user_email} is not registered. \n Please try again\n")
 
             
-def register_user():
+def register_user(mail):
     """
     registers new user email
     """
     clear()
     while True:
-        new_user_email = input("Please enter your email address: ")
+        new_user_email = input("Please enter your email address to register: \n")
         try:
            valid_new_user_email = validate_email(new_user_email,check_deliverability = False)
            print(f"{valid_new_user_email } is a valid email.")
-           SHEET.worksheet("email").append_row([new_user_email])
-           create_new_user_name(new_user_email)
+           new_email_check(new_user_email, mail)
            break
            
-        except EmailNotValidError as e:  
-            print(f"{new_user_email} is not a valid email address.")    
+        except EmailNotValidError:  
+            print(f"{new_user_email} is not a valid email address.")
+            
+            
+def new_email_check(new_user_email, mail):
+    """
+    Checks to see if the entered email is already in use
+    """
+    if new_user_email in mail:
+        print("The email addrress you entered is already in use. \n")
+        print("Please use a different email or login \n")
+        check_email_registered()
+        
+    else:
+        SHEET.worksheet("email").append_row([new_user_email])
+        create_new_user_name(new_user_email)  
                 
 def create_new_user_name(new_email):
     while True:
@@ -95,8 +107,9 @@ def create_new_user_name(new_email):
             SHEET.add_worksheet(title = new_email, rows=100, cols=20)
             worksheet_to_update = SHEET.worksheet(new_email)
             worksheet_to_update.append_row(new_user_name)
-           
-           
+            print(f"{new_user_name[0]} welcome to the Coldroom Capacity Calculator. ")
+            print(f"{new_email} is your registered email.")
+            print("Loading Projects......")
             break
         else:
              print("Your username cannot be bigger than 8 characters!")
